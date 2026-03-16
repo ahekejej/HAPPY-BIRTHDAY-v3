311078d7e620f7752ce7e9a21a71d7a4dae6c2d2
ahekejej <brandshekh1@gmail.com>
2026-03-16 17:13:14 +0530

    Update birthday message in flowers.js

--- flowers.js
+++ flowers.js
@@ -3,7 +3,7 @@ onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
   
-      const titles = ('HAPPY BDAY NADIA ').split('')
+      const titles = ('HAPPY BIRTHDAY NADIA 💖✨ ').split('')
       const titleElement = document.getElementById('title');
       let index = 0;
   
